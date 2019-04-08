02ee117c8f83bd2f15728433c2a30bc07d31ca00
Serkan Yüzbaşıoğlu <33839826+Seko003@users.noreply.github.com>
2019-04-08 21:59:10 +0300

    Add files via upload

---
+++ 10.bölüm (veri tabanı sql)/Personel_Kayit/Personel_Kayit/Frmistatistik.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Personel_Kayit
+{
+    partial class Frmistatistik
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.LblToplamPersonel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.LblEvliPersonelSayisi = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.LblBekarPersonelSayisi = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.LblSehirSayisi = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.LblToplamMaas = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.LblOrtalamaMaas = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(254, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Toplam Personel Sayısı :";
+            // 
+            // LblToplamPersonel
+            // 
+            this.LblToplamPersonel.AutoSize = true;
+            this.LblToplamPersonel.Font = new System.Drawing.Font("Corbel", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblToplamPersonel.Location = new System.Drawing.Point(289, 27);
+            this.LblToplamPersonel.Name = "LblToplamPersonel";
+            this.LblToplamPersonel.Size = new System.Drawing.Size(25, 29);
+            this.LblToplamPersonel.TabIndex = 1;
+            this.LblToplamPersonel.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(69, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(215, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Evli Personel Sayısı :";
+            // 
+            // LblEvliPersonelSayisi
+            // 
+            this.LblEvliPersonelSayisi.AutoSize = true;
+            this.LblEvliPersonelSayisi.Font = new System.Drawing.Font("Corbel", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblEvliPersonelSayisi.Location = new System.Drawing.Point(290, 65);
+            this.LblEvliPersonelSayisi.Name = "LblEvliPersonelSayisi";
+            this.LblEvliPersonelSayisi.Size = new System.Drawing.Size(25, 29);
+            this.LblEvliPersonelSayisi.TabIndex = 3;
+            this.LblEvliPersonelSayisi.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(47, 108);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(237, 29);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Bekar Personel Sayısı :";
+            // 
+            // LblBekarPersonelSayisi
+            // 
+            this.LblBekarPersonelSayisi.AutoSize = true;
+            this.LblBekarPersonelSayisi.Font = new System.Drawing.Font("Corbel", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblBekarPersonelSayisi.Location = new System.Drawing.Point(289, 108);
+            this.LblBekarPersonelSayisi.Name = "LblBekarPersonelSayisi";
+            this.LblBekarPersonelSayisi.Size = new System.Drawing.Size(25, 29);
+            this.LblBekarPersonelSayisi.TabIndex = 5;
+            this.LblBekarPersonelSayisi.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(140, 146);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(144, 29);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Şehir Sayısı : ";
+            // 
+            // LblSehirSayisi
+            // 
+            this.LblSehirSayisi.AutoSize = true;
+            this.LblSehirSayisi.Font = new System.Drawing.Font("Corbel", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblSehirSayisi.Location = new System.Drawing.Point(290, 146);
+            this.LblSehirSayisi.Name = "LblSehirSayisi";
+            this.LblSehirSayisi.Size = new System.Drawing.Size(25, 29);
+            this.LblSehirSayisi.TabIndex = 7;
+            this.LblSehirSayisi.Text = "0";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(126, 180);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(158, 29);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Toplam Maaş :";
+            // 
+            // LblToplamMaas
+            // 
+            this.LblToplamMaas.AutoSize = true;
+            this.LblToplamMaas.Font = new System.Drawing.Font("Corbel", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblToplamMaas.Location = new System.Drawing.Point(289, 180);
+            this.LblToplamMaas.Name = "LblToplamMaas";
+            this.LblToplamMaas.Size = new System.Drawing.Size(25, 29);
+            this.LblToplamMaas.TabIndex = 9;
+            this.LblToplamMaas.Text = "0";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(105, 224);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(179, 29);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "Ortalama Maaş :";
+            // 
+            // LblOrtalamaMaas
+            // 
+            this.LblOrtalamaMaas.AutoSize = true;
+            this.LblOrtalamaMaas.Font = new System.Drawing.Font("Corbel", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblOrtalamaMaas.Location = new System.Drawing.Point(290, 224);
+            this.LblOrtalamaMaas.Name = "LblOrtalamaMaas";
+            this.LblOrtalamaMaas.Size = new System.Drawing.Size(25, 29);
+            this.LblOrtalamaMaas.TabIndex = 11;
+            this.LblOrtalamaMaas.Text = "0";
+            // 
+            // Frmistatistik
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 358);
+            this.Controls.Add(this.LblOrtalamaMaas);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.LblToplamMaas);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.LblSehirSayisi);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.LblBekarPersonelSayisi);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.LblEvliPersonelSayisi);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.LblToplamPersonel);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Corbel", 14F);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Frmistatistik";
+            this.Text = "Frmistatistik";
+            this.Load += new System.EventHandler(this.Frmistatistik_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LblToplamPersonel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label LblEvliPersonelSayisi;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label LblBekarPersonelSayisi;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label LblSehirSayisi;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label LblToplamMaas;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label LblOrtalamaMaas;
+    }
+}
